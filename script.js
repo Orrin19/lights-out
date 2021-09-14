30212a0dfc6d4e7a090850b1b50205b832517d27
Orrin19 <67312910+Orrin19@users.noreply.github.com>
2021-09-14 19:40:41 +0700

    fix of false start

--- script.js
+++ script.js
@@ -4,56 +4,65 @@ const label = document.querySelector('.label');
 var delay;
 var isStarting = false;
 var isAwaiting = false;
+var isBreaking = false;
 var startTime;
 var reactionTime;
+var lightsTime;
+var breakingTime;
 
-const outLights = () => {
-  lights.forEach((light) => {
-    light.style.setProperty('--current-color', '#777');
-  });
-  startTime = new Date().getTime();
-  isStarting = false;
-  isAwaiting = true;
-  return startTime;
-};
-
-const setResult = (reactionTime) => {
-  label.innerHTML = `Your reaction time is ${reactionTime} milliseconds!<br>Click to start again.`;
-  isAwaiting = false;
-};
-
-const endAwaiting = (startTime) => {
+const end = (startTime) => {
   var endTime = new Date().getTime();
   reactionTime = endTime - startTime;
-  return setResult(reactionTime);
+  label.innerHTML = `Your reaction time is ${reactionTime} milliseconds!<br>Click to start again.`;
+  isAwaiting = false;
+  return;
 };
 
 const falseStart = () => {
-  label.innerHTML = 'False start!<br>Click in a few seconds to try again.';
+  label.innerHTML = 'False start!<br>Wait for a few seconds to try again.';
   lights.forEach((light) => {
     light.style.setProperty('--current-color', '#777');
   });
   isStarting = false;
   isAwaiting = false;
+  isBreaking = true;
+  breakingTime = delay - new Date().getTime() + lightsTime;
+  setTimeout(() => {
+    isBreaking = false;
+    label.innerHTML = 'Click here to start countdown!';
+  }, breakingTime);
 };
 
 const start = () => {
   isStarting = true;
+  lightsTime = new Date().getTime();
   label.innerHTML = 'Click when the lights go out!';
   lights.forEach((light, index) => {
     setTimeout(() => {
-      if (!isStarting) return;
+      if (!isStarting || isBreaking) return;
       light.style.setProperty('--current-color', '#d6281d');
     }, (index + 1) * 1000);
   });
-  var delay = Math.floor(Math.random() * 3000 + 5800);
+  delay = Math.floor(Math.random() * 3000 + 5800);
   console.log('delay before out:', delay - 5000);
   setTimeout(() => {
     if (!isStarting) return;
-    outLights(delay);
+    lights.forEach((light) => {
+      light.style.setProperty('--current-color', '#777');
+    });
+    startTime = new Date().getTime();
+    isStarting = false;
+    isAwaiting = true;
+    return startTime;
   }, delay);
 };
 
 action.addEventListener('click', () => {
-  isAwaiting ? endAwaiting(startTime) : isStarting ? falseStart() : start();
+  isAwaiting
+    ? end(startTime)
+    : isStarting
+    ? falseStart()
+    : isBreaking
+    ? ''
+    : start();
 });
